643ddf409b502703f55b3911e50749aae96f3b59
Luiz Paulo Souza <luizuksouza@gmail.com>
2021-07-27 14:05:00 -0300

    First Commit: CardGame Creation

---
+++ main.js
@@ -0,0 +1,24 @@
+import './src/styles/settings/colors.css';
+import './src/styles/generic/reset.css';
+import './src/styles/elements/base.css';
+
+import CardGame from "./src/components/CardGame";
+
+
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame();
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+
+// 1) jQuery
+
+// 2) consumindo uma api do DOM pra buscar o elemento HTML através da seu id e guardar na variável #root
+
+// 3) BOA PRÁTICA: Toda variável que aguardar um elemento da tela (Html render) deve ter o $ como antecipação.
+
+//Root é a representação da div em formato HTML.
+
+//Não usar innerHTML por causa de segurança.
+
+// insertAdjacentHTML ("primeiro parametro são 4 possibilidades e diz respeito ao posicionamento do html inserido em relação à div", segundo parâmetro é a string html do nosso código
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+//similar ao JSX
+
+function CardGame () {
+    return `
+    <article class="card-game">
+        <img src="images/alura-pixel.png" alt="Logo da Alura">
+    </article>
+    `
+}
+
+export default CardGame;
