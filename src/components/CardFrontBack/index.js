7236a6a0d2a95055ae03567e549287dd0d632cbb
Luiz Paulo Souza <luizuksouza@gmail.com>
2021-07-28 10:40:56 -0300

    Add event rotate card when CardFrontBack was clicked.

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,27 @@
 import CardGame from "../CardGame";
 import './style.css';
 
+
+
 function CardFrontBack () {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target; //target é o elemento html origem do click;
+        const $cardFrontBack = $origin.closest('.card-front-back');
+
+        // if ($cardFrontBack.classList.contains('-active')) {
+        //     $cardFrontBack.classList.remove('-active');
+        // } else {
+        //     $cardFrontBack.classList.add('-active')
+        // toggle faz a função desse if inteiro, verifica se tem a classe, se tiver ele remove, se não tiver ele adiciona.}
+
+        $cardFrontBack.classList.toggle('-active');
+
+    }
+
 
     return /*html*/`
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
