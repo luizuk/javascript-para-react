af26aee6a2989aed835912054fbb5a42162fc6c1
Luiz Paulo Souza <luizuksouza@gmail.com>
2021-08-03 09:26:11 -0300

    Work in progress: Create VsPlayer component without CSS

---
+++ src/components/VsPlayer/index.js
@@ -0,0 +1,7 @@
+export default function VsPlayer(content = "vs") {
+    return /*html*/`
+        <span class="vs-player">${content}</span>
+    `
+}
+
+//$ aparece pq dentro de template string (crases)
